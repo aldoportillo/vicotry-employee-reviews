568ce6b84415fdf26551c4f0da79dfe933bb126a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-13 09:52:12 -0600

    Run migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id            :integer          not null, primary key
+#  content       :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  restaurant_id :integer
+#  user_id       :integer
+#
 class Post < ApplicationRecord
 end
