1def6976e7634f49e31f4ef030b46b5c3bc776fa
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-13 11:15:06 -0600

    Add admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_144843) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_13_171356) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.integer "restaurant_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_144843) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "restaurant_id"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
